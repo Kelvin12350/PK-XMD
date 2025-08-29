7f39249c90cf4f7199519d5ef8ffc667f712fbf5
Heatless <dianwilson66@gmail.com>
2025-08-29 04:46:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "pk~awJxQYjZ#WBQs9yi-CGjyheT3zuVaRKE2YGnIIeXLliDFAozZYtM",
+SESSION_ID: process.env.SESSION_ID || "nexus~LowQmSpY#lnawfq63l_hNnYluTl86dCGO6f0A9LOJTWR2l0Kad8w",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
